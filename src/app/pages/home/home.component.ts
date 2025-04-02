9fb6640ba9b026a67721cde94b5c0dad83ac76a0
NbglRoot <naimbgdl2003@gmail.com>
2025-04-02 18:44:21 +0200

    header and footer design done | router-routes not implemented | footer needs redesign of social media icons

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {}
